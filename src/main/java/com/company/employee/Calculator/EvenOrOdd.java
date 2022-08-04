98eac7db9503bcd74d28326ce2238c7358c899e6
priyankatriwish <priyanka@triwish.io>
2022-08-04 19:57:31 +0530

    even or odd

---
+++ src/main/java/com/company/employee/Calculator/EvenOrOdd.java
@@ -0,0 +1,11 @@
+package com.company.employee.Calculator;
+
+public class EvenOrOdd {
+    public String evenOrOdd(int num) {
+        if (num % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+}
